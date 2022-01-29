ab80fc751f82281075dd8cd2235aa42cedbbc770
Kiritow <1362050620@qq.com>
2022-01-29 10:19:46 +0000

    Update install scripts

--- install.sh
+++ install.sh
@@ -22,6 +22,10 @@ tar -xvzf udpspeeder.tgz speederv2_amd64
 chmod +x speederv2_amd64
 rm udpspeeder.tgz
 
+curl -vL https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz -o gost.gz
+gzip -d gost.gz
+chmod +x gost
+
 cd ..
 
 VERIFIED_TUNNEL_HASH="a7ce38b2c30980be4e71c3af8a9c1db8183db349c699fa6f843e67add7e6cca2"
@@ -29,6 +33,9 @@ LOCAL_TUNNEL_HASH=$(sha256sum bin/udp2raw_amd64 | awk '{print $1}')
 
 VERIFIED_SPEEDER_HASH="3cf8f6c1e9baa530170368efb8a4bfcd6e75f88c2726ecbf2a75261dd1dd9fd5"
 LOCAL_SPEEDER_HASH=$(sha256sum bin/speederv2_amd64 | awk '{print $1}')
+
+VERIFIED_GOST_HASH="5434f730594d29b933087dcaf1ae680bee7077abd021c004f28287deccfe49b5"
+LOCAL_GOST_HASH=$(sha256sum bin/speederv2_amd64 | awk '{print $1}')
 
 if [ "$LOCAL_TUNNEL_HASH" == "$VERIFIED_TUNNEL_HASH" ]
 then
@@ -42,4 +49,11 @@ then
     echo "[OK] speederv2 hash match: $LOCAL_SPEEDER_HASH"
 else
     echo "[WARN] speederv2 hash mismatch: $LOCAL_SPEEDER_HASH. Expected: $VERIFIED_SPEEDER_HASH"
+fi
+
+if [ "$LOCAL_GOST_HASH" == "$VERIFIED_GOST_HASH" ]
+then
+    echo "[OK] gost hash match: $LOCAL_GOST_HASH"
+else
+    echo "[WARN] gost hash mismatch: $LOCAL_GOST_HASH. Expected: $VERIFIED_GOST_HASH"
 fi
--- install_cn.sh
+++ install_cn.sh
@@ -18,8 +18,10 @@ cd wg-op-binary
 
 openssl enc -aes-256-cbc -pbkdf2 -a -d -in bin.01 -out ../udp2raw_amd64
 openssl enc -aes-256-cbc -pbkdf2 -a -d -in bin.02 -out ../speederv2_amd64
+openssl enc -aes-256-cbc -pbkdf2 -a -d -in bin.03 -out ../gost
 chmod +x ../udp2raw_amd64
 chmod +x ../speederv2_amd64
+chmod +x ../gost
 
 cd ..
 rm -rf wg-op-binary
@@ -31,6 +33,9 @@ LOCAL_TUNNEL_HASH=$(sha256sum bin/udp2raw_amd64 | awk '{print $1}')
 
 VERIFIED_SPEEDER_HASH="3cf8f6c1e9baa530170368efb8a4bfcd6e75f88c2726ecbf2a75261dd1dd9fd5"
 LOCAL_SPEEDER_HASH=$(sha256sum bin/speederv2_amd64 | awk '{print $1}')
+
+VERIFIED_GOST_HASH="5434f730594d29b933087dcaf1ae680bee7077abd021c004f28287deccfe49b5"
+LOCAL_GOST_HASH=$(sha256sum bin/speederv2_amd64 | awk '{print $1}')
 
 if [ "$LOCAL_TUNNEL_HASH" == "$VERIFIED_TUNNEL_HASH" ]
 then
@@ -44,4 +49,11 @@ then
     echo "[OK] speederv2 hash match: $LOCAL_SPEEDER_HASH"
 else
     echo "[WARN] speederv2 hash mismatch: $LOCAL_SPEEDER_HASH. Expected: $VERIFIED_SPEEDER_HASH"
+fi
+
+if [ "$LOCAL_GOST_HASH" == "$VERIFIED_GOST_HASH" ]
+then
+    echo "[OK] gost hash match: $LOCAL_GOST_HASH"
+else
+    echo "[WARN] gost hash mismatch: $LOCAL_GOST_HASH. Expected: $VERIFIED_GOST_HASH"
 fi
